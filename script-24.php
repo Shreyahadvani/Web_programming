a2689276549ce0edfd63d4b4f04bfc632e0e9b04
Shreyahadvani <154863965+Shreyahadvani@users.noreply.github.com>
2024-01-16 12:07:37 +0530

    Add files via upload

---
+++ script-24.php
@@ -0,0 +1,5 @@
+<?php  
+$num=625;  
+echo "Number is : $num".'<br>';  
+echo "sqrt function number is : ".sqrt ($num);  
+?>  
---
+++ script-25.php
@@ -0,0 +1,10 @@
+<?php  
+$a = 34;  
+$b = 58;  
+// Swapping Logic  
+$third = $a;  
+$a = $b;  
+$b = $third;  
+echo "After swapping:<br><br>";  
+echo "a =".$a."  b=".$b;  
+?>  
